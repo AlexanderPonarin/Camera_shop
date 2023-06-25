f83a0cc5c804daa9a9b3699743ea34061f35e537
AlexanderPonarin <114759942+AlexanderPonarin@users.noreply.github.com>
2023-06-25 23:00:51 +0300

    Update product.tsx

--- project/src/components/product/product.tsx
+++ project/src/components/product/product.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { Products } from '../../types/products';
 import ProductScreen from '../../pages/product-screen/product-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import { ProductTabsNameSpace } from '../../consts';
 
 type ProductsProps = {
     products: Products;
@@ -9,6 +10,11 @@ type ProductsProps = {
 
 function Product({products}: ProductsProps): JSX.Element {
   const params = useParams();
+  const { tab } = useParams();
+  console.log(tab)
+  if (tab) {
+    tab === ProductTabsNameSpace.
+  }
   const product = products?.find((item) => String(item.id) === params?.id);
   return product ? <ProductScreen product={product} /> : <NotFoundScreen />;
 }
